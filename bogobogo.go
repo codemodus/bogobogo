cee89f357c98f34db051630d1031f2236ba651f8
Daved <codingmusica@gmail.com>
2015-05-31 02:19:19 -0700

    Removed useless code, increased sample span, and improved error.

--- bogobogo.go
+++ bogobogo.go
@@ -6,7 +6,7 @@ import (
 )
 
 func MIPS() (float64, error) {
-	t := time.Millisecond * 33
+	t := time.Millisecond * 75
 	for l := uint64(1); l > 0; l <<= 1 {
 		s := time.Now()
 		for i := uint64(0); i < l; i++ {
@@ -14,19 +14,9 @@ func MIPS() (float64, error) {
 		e := time.Now().Sub(s)
 
 		if e > t {
-			for l2 := l >> 1; l2 < l; l2 = ((l2 + l) / 2) + l2/2 {
-				s = time.Now()
-				for i := uint64(0); i < l2; i++ {
-				}
-				e2 := time.Now().Sub(s)
-				if e2 > t {
-					l = l2
-					e = e2
-				}
-			}
 			bm := (float64(l) / (float64(e) / 1e+9) / 1e+6)
 			return float64(int(bm*100)) / 100, nil
 		}
 	}
-	return 0, errors.New("failed")
+	return 0, errors.New("unable to calculate bogobogomips")
 }
